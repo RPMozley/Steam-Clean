7848fd8209910eb973ed6ae058f2321a9d59fe2b
RPM <75445940+RPMozley@users.noreply.github.com>
2025-04-13 23:11:20 +0100

    script & readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Steam Clean
- Clean up the shared memory Steam client leaves behind
+ Clean up the shared memory the Steam client leaves behind.
+ 
+ ###
+ https://github.com/ValveSoftware/steam-for-linux/issues/9233
---
+++ steam_clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# https://github.com/ValveSoftware/steam-for-linux/issues/9233
+# Example: /dev/shm/u1000-Shm_0a1b2c3d
+
+for f in /dev/shm/u"$UID"*; do
+	# Ignore find error if no u1* exists.
+	file="$(find "${f}" -type f 2>/dev/null)"
+	if [ -n "${file}" ]; then
+		if fuser "$file" >/dev/null 2>/dev/null; then
+			continue
+		fi
+		rm -Iv "$file"
+		#echo "Deleted $file"
+	fi
+done;
